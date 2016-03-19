a718d2198016dc10b36ecd60299be01c0108804f
EduardoAC <eduardo@dreammakerfactory.com>
2016-03-19 13:10:22 +0000

    Push add account

--- app/scripts/controllers/add-account.js
+++ app/scripts/controllers/add-account.js
@@ -8,24 +8,27 @@
  * Controller of the trustScoreAppApp
  */
 angular.module('trustScoreAppApp')
-  .controller('AddAccountCtrl', function ($scope) {
-      var container = document.getElementById("main-container");
-      container.classList.add("new-account");
-      container.querySelector("article").style.height = "100%";
-      container.querySelector("article").style.position = "relative";
-      $scope.events = {};
-      $scope.events.addAccount = function(){
-          $("#model-add-account").modal({
-              keyboard: false
-          });
-      };
-  }).directive("mAddAccount",function(){
+        .controller('AddAccountCtrl', function ($scope) {
+            var container = document.getElementById("main-container");
+            container.classList.add("new-account");
+            container.querySelector("article").style.height = "100%";
+            container.querySelector("article").style.position = "relative";
+            $scope.events = {};
+            $scope.events.addAccount = function () {
+                $("#model-add-account").modal({
+                    keyboard: false
+                });
+            };
+            $scope.$on("$destroy", function () {
+                container.classList.remove("new-account");
+            });
+        }).directive("mAddAccount", function () {
 
-        return {
-            restrict: 'E',
-            scope: {
-                postInfo: '='
-            },
-            templateUrl: 'templates/modal-add-account.html'
-        };
-  });
+    return {
+        restrict: 'E',
+        scope: {
+            postInfo: '='
+        },
+        templateUrl: 'templates/modal-add-account.html'
+    };
+});
