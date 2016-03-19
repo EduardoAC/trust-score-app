e479090be4fbed778307f40dcd5da5df2cc1b085
Jacek Burys <jsb314@ic.ac.uk>
2016-03-19 20:41:37 +0000

    auth factory

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,46 @@
  * Controller of the trustScoreAppApp
  */
 angular.module('trustScoreAppApp')
+.factory('auth', ['$http', '$window', function($http, $window){
+  var auth = {};
+  auth.saveToken = function(token) {
+    $window.localStorage['trust-score-token'] = token;
+  };
+  auth.getToken = function() {
+    return $window.localStorage['trust-score-token'];
+  };
+  auth.isLoggedIn = function() {
+    var token = auth.getToken();
+
+    if(token) {
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  };
+  auth.currentUser = function(){
+    if(auth.isLoggedIn()) {
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+      return payload.username;
+    }
+  };
+  auth.register = function(user) {
+    return $http.post('/register', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+  auth.logIn = function(user) {
+    return $http.post('/login', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+  auth.logOut = function() {
+    $window.localStorage.removeItem('trust-score-token');
+  };
+  return auth;
+}])
   .controller('MainCtrl', function ($scope) {
     $scope.events = {};
     $scope.events.moreDetails = function(event){
