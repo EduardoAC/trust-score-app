3f39c604215dd4a605663b9fe96bb2eb5bf69747
EduardoAC <eduardo@dreammakerfactory.com>
2016-03-20 08:18:04 +0000

    Changes to the UI

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -8,8 +8,10 @@
  * Controller of the trustScoreAppApp
  */
 angular.module('trustScoreAppApp')
-.controller('RegisterCtrl', [
-'$scope',
-function ($scope) {
-  
+.controller('RegisterCtrl', ['$scope', function ($scope) {
+    $scope.master = {};
+
+    $scope.update = function(user) {
+      $scope.master = angular.copy(user);
+    };
 }]);
